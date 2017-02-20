571d38360d54d5bbfbb2eddabf7bb55d16c84bf3
Andre Cesar de Jesus Gaspar - 81717626 <81717626@animaedu.intranet>
2017-02-20 20:43:38 -0300

    segundo exercicio

---
+++ Aula JAva/solucao2.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class solucao2{
+   public static void main (String args []){
+   String palavra = JOptionPane.showInputDialog
+      ("digite um numero!");
+      
+   JOptionPane.showMessageDialog(null, palavra);
+   }
+}
