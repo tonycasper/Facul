2b998279d87e3f1faaae6536d68fa20d5e192e7d
Andre Cesar de Jesus Gaspar - 81717626 <81717626@animaedu.intranet>
2017-02-20 20:07:26 -0300

    first

--- Aula JAva/Solucao1.java
+++ Aula JAva/Solucao1.java
@@ -1,5 +1,11 @@
+import javax.swing.JOptionPane;
+
 public class Solucao1{
    public static void main (String args []){
       String palavra1 = JOptionPane.showInputDialog("Digite a primeira palavra");
+
+      String palavra2 = JOptionPane.showInputDialog("Digite a seguunda palavra");
+      
+      String palavra3 = JOptionPane.showInputDialog("Digite a terceira palavra");
    }
 }
