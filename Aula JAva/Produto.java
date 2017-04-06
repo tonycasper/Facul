83c82b7d820f8df6ba53c96c8ca81d4ae89d7576
Andre Cesar de Jesus Gaspar - 81717626 <81717626@animaedu.intranet>
2017-04-05 22:28:47 -0300

    aula 5 final

--- Aula JAva/Produto.java
+++ Aula JAva/Produto.java
@@ -11,10 +11,10 @@ public class Produto {
   public String getNome(){
    return this.nome;
   }
-  public double preco(){
+  public double getPreco(){
    return this.preco;
   }
-  public Etiqueta(){
+  public getEtiq(){
    return this.etiq;
   }
   public void setNome(String n){
--- Aula JAva/TesteProduto.java
+++ Aula JAva/TesteProduto.java
@@ -2,22 +2,26 @@ import javax.swing.JOptionPane;
 public class TesteProduto{
    public static void main (String args[]){
       //variaveis e objetos
-      String marca, tamanho;
-      double preco;
-
+      String nome,marca, tamanho,msg = " ", preco;
+//      DecimalFormat df2 = new DecimalFormat("#,##0.00");
       //solicita valores ao usuario
       nome = JOptionPane.showInputDialog("Qual o Nome?");
       marca = JOptionPane.showInputDialog("Qual a marca cachoro?");
       tamanho = JOptionPane.showInputDialog("Qual o tamanho?");
-      preco = double.parseDouble(JOptionPane.showInputDialog("Qual o preco $$"));
+      preco =JOptionPane.showInputDialog("Qual o preco $$");
       //criar etiqueta depois do produto
       Etiqueta e1 = new Etiqueta(marca,tamanho);
       Produto p1 = new Produto();
-      
+      //altera conteudo de produto
       p1.setNome(nome);
       p1.setMarca(marca);
       p1.setEtiq(e1);
-
+      //cria string para exibir conteudo
+      msg = "\nNome: " + p1.getNome() + 
+      "\nMarca: " +  p1.getEtiq().p1.getMarca()+
+      "\nPreco: " + p1.getPreco().p1.getPreco();
+      
+      JOptionPane.showInputDialog(null,msg);
       System.exit(0);
    }
 }
